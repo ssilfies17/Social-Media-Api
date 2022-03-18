b9b6bcec106beeb643bee8ef0a4e5adddb3ac646
Sawyer Silfies <ssilfies17@gmail.com>
2022-03-17 23:22:25 -0700

    User controller created and user routes started.

--- Main/routes/api/userRoutes.js
+++ Main/routes/api/userRoutes.js
@@ -1,1 +1,14 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser).post(updateUser);
+
+module.exports = router;
